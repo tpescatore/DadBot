3a9b08aff76b2b8977e87a6fb5a07addce2c9cc6
Yaikwhed <yaikwhed@aim.com>
2016-09-28 19:54:22 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,7 +15,7 @@ function respond() {
       //botRegexDaf = /^\/dafuq/;
       //siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7'
       //siege3 = 'https://i.groupme.com/960x960.png.006e180e05d841c6a2962e844bf1e6fd';
-	  botRegex=/^\/kya!~/;
+	  botRegexKya=/^\/kya!~/;
   //var teamAb = ["NE","NO","ARI","PHI","CLE","TEN","OAK","DAL","IND","SEA","CIN","PIT","JAC"
   //              ,"BAL","SD","DEN","MIN","ATL","KC","NYG","GB","DET","HOU","STL","CHI","CAR",
   //              "MIA","BUF","SF","WAS","NYJ","TB"]
@@ -24,7 +24,7 @@ function respond() {
     postMessage(cool());
     this.res.end();
   }
-  else if(request.text && botRegexf.test(request.text)) {
+  else if(request.text && botRegexKya.test(request.text)) {
 	  this.res.writeHead(200);
 	  postMessage("B-b-baka!");
 	  this.res.end();
