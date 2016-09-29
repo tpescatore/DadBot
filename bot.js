f6d0588ac8b90ef4ad84d35695d74f7d47787bac
Yaikwhed <yaikwhed@aim.com>
2016-09-28 23:42:19 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ function respond() {
 
   if(request.text && botRegexKya.test(request.text)) {
     this.res.writeHead(200);
-    postMessage(waifuPhrases.length.toString());
+    postMessage(waifuPhrases[getRandomInt(0,waifuPhrases.length-1)]);
     this.res.end();
   }
   else {
