677c5405f3e2cfaabd976587a9a9cfa1a944b57f
Yaikwhed <yaikwhed@aim.com>
2016-11-23 13:53:44 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,6 +5,9 @@ var botID = process.env.BOT_ID;
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegexDad = /^(I|i)'?m([0-9a-zA-Z"-]|,|'| )*(\.|\?|\!|)/; 
+  var dadToMom = ["This is why I'm always in my den.", "Oh yeah? Well, your meatloaf sucks.", "I'm only with you for the kids.", "I'm happy with blueballs if it means you have to keep your shirt on.",
+                  "The chicken you make is as leathery as your skin.", "Would you shut up and bring me a beer?", "There's two things you and a football have in common: 1. I'd like to kick you hard as possible. 2. You've both been man handled by an obscene number of men.",
+                  "If I ever go gay, it's your fault."]
 
 
   if(request.text && botRegexDad.test(request.text) && (request.text.indexOf("@") == -1) && (request.name.toUpperCase() != "GroupMe".toUpperCase()) && (request.name != "Dad") && textCheck(request.text, botRegexDad) == 1) {
@@ -12,6 +15,11 @@ function respond() {
     postMessage("Hi, " + getResponseString(request.text, botRegexDad) + ". I'm Dad!");
     this.res.end();
   }
+  else if(request.name == "Mom" && getRandomInt(0,1) == 1){
+    this.res.writeHead(200);
+    
+    this.res.end();
+  }  
   else {
     console.log("Nothing happened");
     this.res.writeHead(200);
